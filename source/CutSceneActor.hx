66276703faf852f9a0c83d3a9892848ec69e9c67
Simon Weis <laguna.ws@gmail.com>
2016-09-19 18:24:16 +0200

    adding dance animation

--- source/CutSceneActor.hx
+++ source/CutSceneActor.hx
@@ -34,7 +34,7 @@ class CutSceneActor extends FlxSprite
 		switch (name)
 		{
 			case "dino":
-				trace("load dino");
+				//trace("load dino");
 				this.loadGraphic(AssetPaths.dino__png, true, 24, 18);
 				this.animation.add("idle", [5,5,5,4,5,5,5,5,5,6,7,8,9], 4);
 				this.animation.add("walk", [0, 1, 2, 3], 4);
@@ -46,21 +46,23 @@ class CutSceneActor extends FlxSprite
 				
 			case "dinobag":
 				this.loadGraphic(AssetPaths.dino_bag__png, true, 24, 18);
-				this.animation.add("idle", [5,5,5,4,5,5,5,5,5,6,7,8,9], 4);
+				this.animation.add("idle", [5, 5, 5, 4, 5, 5, 5, 5, 5, 6, 7, 8, 9], 4);
+				this.animation.add("idle2", [45, 46, 47,48,49,50], 3);
 				this.animation.add("walk", [0, 1, 2, 3], 4);
 				this.animation.add("sleep", [10, 11, 12, 13], 4, true);
-				this.animation.add("wakeup", [14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37], 5, false);
+				this.animation.add("wakeup", [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 33, 34,35,36, 37], 5, false);
+				this.animation.add("dance", [39,40,41,42,43,44], 5, true);
 				this.animation.play("idle");
 				setFacingFlip(FlxObject.LEFT, false, false);
 				setFacingFlip(FlxObject.RIGHT, true, false);
 			case "overlay":
-				trace("load overlay");
+				//trace("load overlay");
 				this.makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
 				this.scrollFactor.set();
 				this.color.setRGB(0, 0, 0);
 				this.cameras = [GP.CameraOverlay];
 			case "invis":
-				trace ("load invis");
+				//trace ("load invis");
 				this.makeGraphic(1, 1, FlxColor.TRANSPARENT);
 				
 			default:
