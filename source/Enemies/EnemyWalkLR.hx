32f9196ba9f2922ae47e84aa1c5c54481c6fd97a
Simon Weis <laguna.ws@gmail.com>
2017-05-06 11:12:08 +0200

    adding fox enemy

--- source/Enemies/EnemyWalkLR.hx
+++ source/Enemies/EnemyWalkLR.hx
@@ -1,4 +1,5 @@
 package;
+import flixel.FlxObject;
 import flixel.util.FlxColor;
 
 /**
@@ -15,9 +16,16 @@ class EnemyWalkLR extends BasicEnemy
 	public function new(X: Float, Y: Float) 
 	{
 		super(X, Y);
-		this.makeGraphic(GP.WorldTileSizeInPixel, GP.WorldTileSizeInPixel, FlxColor.RED);
-		_sprite.makeGraphic(GP.WorldTileSizeInPixel, GP.WorldTileSizeInPixel, FlxColor.RED);
+		//this.makeGraphic(GP.WorldTileSizeInPixel, GP.WorldTileSizeInPixel, FlxColor.RED);
+		//_sprite.makeGraphic(GP.WorldTileSizeInPixel, GP.WorldTileSizeInPixel, FlxColor.RED);
 		
+		this.makeGraphic(20, 10, FlxColor.RED);
+		_sprite.loadGraphic(AssetPaths.Enemy_FOX__png, true, 20, 16);
+		_sprite.animation.add("walk", [0, 1, 2, 3], 7);
+		_sprite.animation.play("walk");
+		//_sprite.offset.set(0, -6);
+		_sprite.setFacingFlip(FlxObject.LEFT, true, false);
+		_sprite.setFacingFlip(FlxObject.RIGHT, false, false);
 		_startingPos = X;
 	}
 	
@@ -25,9 +33,10 @@ class EnemyWalkLR extends BasicEnemy
 	{
 		//trace(this.x);
 		super.update(elapsed);
-		
+		//_sprite.setPosition(x, 
 		if (_walkingRight)
 		{
+			_sprite.facing = FlxObject.LEFT;
 			this.velocity.x = GP.EnemyWalkLRSpeed;
 			if (this.x - _startingPos > distance)
 			{
@@ -37,6 +46,7 @@ class EnemyWalkLR extends BasicEnemy
 		}
 		else
 		{
+			_sprite.facing = FlxObject.RIGHT;
 			this.velocity.x = -GP.EnemyWalkLRSpeed;
 			if (this.x - _startingPos < 0)
 			{
