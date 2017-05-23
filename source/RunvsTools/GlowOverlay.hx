ce384c922aa2e182744c7f801172feda18eda4e9
Simon Weis <laguna.ws@gmail.com>
2017-05-23 17:25:12 +0200

    removing cam member from glowoverlay

--- source/RunvsTools/GlowOverlay.hx
+++ source/RunvsTools/GlowOverlay.hx
@@ -12,18 +12,19 @@ import flixel.util.FlxColor;
 class GlowOverlay extends FlxSprite
 {
 
-	var _cam : FlxCamera;
+	//var _cam : FlxCamera;
 	
 	// x and y in Coordinates
 	public function new(X:Float = 0, Y:Float = 0, c: FlxCamera, s: Int, rangescale : Float, exponent:Float) 
 	{
 		super(X, Y);
 		
-		_cam = c;
+		//_cam = c;
 		
 		this.makeGraphic(s, s, FlxColor.WHITE, true);
 		this.offset.set(s / 2, s / 2);
-		var distmax = s/2;// Math.sqrt(this.width * this.width / 4 + this.height * this.height / 4);
+		var distmax = s / 2;
+		
 		for (i in 0... Std.int(this.width))
 		{
 			for (j in 0... Std.int(this.height))
@@ -31,11 +32,13 @@ class GlowOverlay extends FlxSprite
 				var dx = this.width / 2 - i;
 				var dy = this.height / 2 - j;
 				var dist = dx * dx + dy * dy;
-				var a = 1.0 - Math.pow(dist / distmax/distmax / rangescale/rangescale, exponent);
+				var a = 1.0 - Math.pow(dist / distmax / distmax / rangescale / rangescale, exponent);
+				//trace(a);
+				
 				this.pixels.setPixel32(i, j, FlxColor.fromRGBFloat(1,1,1, a));
 			}
 		}
-		this.cameras = [_cam];
+		this.cameras = [c];
 	}
 	
 }
